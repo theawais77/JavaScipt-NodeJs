39b7a443619cd27b2cf07bd5a86de1d502d70170
Awais Tahir <theowais456@gmail.com>
2024-08-29 11:55:09 +0500

    first commit

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,23 @@
+const accountId = 45;
+let accountEmail = "owais@gmail.com";
+var accountPassword = "1234";
+accountCity = "Skt";
+
+let accountState;
+
+
+
+accountEmail="jkaks";
+accountPassword="a5678";
+accountCity="rwp";
+
+/*
+Prefer not to use var
+issue in block scope
+
+
+*/
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
