2697dbcf6efd3095a1a2f8e45670c9c61ebe812a
Awais Tahir <theowais456@gmail.com>
2025-01-24 15:17:37 +0500

    1

--- 01_basic/mini_array.js
+++ 01_basic/mini_array.js
@@ -2,3 +2,4 @@ const marvel_heroes=["thor","Ironman","spiderman"];
 const dc_heroes=["superman","flash","batman"];
  marvel_heroes.push(dc_heroes);
 console.log(marvel_heroes);
+console.table(dc_heroes)
